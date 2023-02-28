3c8b1f08988bd5b29916675ef826426a7086c2ef
Netson Cavina <netsoncavina@gmail.com>
2023-02-28 19:35:00 -0300

    adicionados imports

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
