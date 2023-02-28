9f904405ec9ff64b6300e7a89eae21e2cf8a52ab
Netson Cavina <netsoncavina@gmail.com>
2023-02-28 19:54:40 -0300

    adição das rotas de User

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
