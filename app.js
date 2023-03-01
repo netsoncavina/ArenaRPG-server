00cf61d8e8f6919e228bf609c6c2a24bcad88495
Netson Cavina <netsoncavina@gmail.com>
2023-03-01 20:10:33 -0300

    adição das rotas na aplicação

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
