8992e0ee060987418f067d6972a14eca95ce41c8
Netson Cavina <netsoncavina@gmail.com>
2023-03-01 20:10:24 -0300

    desenvolvimento das rotas

---
+++ routes/postRoutes.js
@@ -0,0 +1,75 @@
+import express from "express";
+import Post from "../models/Post.js";
+
+const router = express.Router();
+
+// POST
+router.post("/", async (req, res) => {
+  try {
+    const post = await Post.create(req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// GET
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get("/user/:author", async (req, res) => {
+  try {
+    const post = await Post.find({ author: req.params.author });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get("/post/:type", async (req, res) => {
+  try {
+    const post = await Post.find({ type: req.params.type });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+// PATCH
+router.patch("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+export default router;
