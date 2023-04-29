403453f711a954b6da3c3e2385ba905168496bf7
Netson Cavina <netsoncavina@gmail.com>
2023-04-28 21:40:30 -0300

    Adicionado se o post foi editado

--- models/Post.js
+++ models/Post.js
@@ -29,7 +29,10 @@ const postSchema = new mongoose.Schema({
     type: [Object],
     default: [],
   },
-
+  edited: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
